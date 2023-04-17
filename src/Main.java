98e7157ad570316b32868288f32cf4bf7b3a40f6
gem00n <o.ghanaiem@student.aaup.edu>
2023-04-17 23:48:14 +0300

    Moves Zero's:

--- src/Main.java
+++ src/Main.java
@@ -1,80 +1,25 @@
-abstract class Animal {
-  private String name;
-  private int age;
+//Moves Zero's: 
+import java.util.*;
 
-  public Animal(String name, int age) {
-    this.name = name;
-    this.age = age;
+class Main {
+  public static void moveZeros(int[] array) {
+    int count = 0;
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] != 0) {
+        array[count] = array[i];
+        count++;
+      }
+    }
+    while (count < array.length) {
+      array[count] = 0;
+      count++;
+    }
+    System.out.println(Arrays.toString(array));
+
   }
 
-  public String getName() {
-    return name;
-  }
-
-  public int getAge() {
-    return age;
-  }
-
-  public abstract String getSound();
-
-  public String toString() {
-    return getName() + "is " + getAge() + "years old";
-  }
-}
-
-class Cat extends Animal {
-  private String breed;
-
-  public Cat(String name, int age, String breed) {
-    super(name, age);
-    this.breed = breed;
-  }
-
-  public String getBreed() {
-    return breed;
-  }
-
-  public String getSound() {
-    return "Meow";
-  }
-}
-
-class Dog extends Animal {
-  private String breed;
-
-  public Dog(String name, int age, String breed) {
-    super(name, age);
-    this.breed = breed;
-  }
-
-  public String getBreed() {
-    return breed;
-  }
-
-  public String getSound() {
-    return "Woof";
-  }
-}
-
-interface Pettable {
-  public void pet();
-}
-
-class PettableCat extends Cat implements Pettable {
-  public PettableCat(String name, int age, String breed) {
-    super(name, age, breed);
-  }
-
-  public void pet() {
-    System.out.println("Petting " + getName() + " the " + getBreed() + "dog");
-  }
-}
-
-public class Main {
   public static void main(String[] args) {
-    Cat cat = new Cat("Sara", 1, "Siamese");
-    Dog dog = new Dog("Owen", 2, "Huskey");
-    System.out.println("The Cat Name Is: " + cat.getName() + " The Cat Age Is " + cat.getAge());
-    System.out.println(cat.getSound());
+    int[] array = { 1, 0, 2, 0, 3, 0, 4 };
+    moveZeros(array);
   }
 }
