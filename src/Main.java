15bdf77a8506a924cae7e5520a7e4a8138cb4c8e
gem00n <o.ghanaiem@student.aaup.edu>
2023-04-18 02:34:20 +0300

    fizzBuzz's:

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,23 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Main {
-  public static void mergeAndSort(int[] arr1, int[] arr2) {
-    int[] arr3 = new int[arr1.length + arr2.length];
-    System.arraycopy(arr1, 0, arr3, 0, arr1.length);
-    System.arraycopy(arr2, 0, arr3, arr1.length, arr2.length);
-    System.out.println(Arrays.toString(arr3));
-    int temp = 0;
-    for (int i = 0; i < arr3.length; i++) {
-      for (int j = i + 1; j < arr3.length; j++) {
-        if (arr3[i] > arr3[j]) {
-          temp = arr3[i];
-          arr3[i] = arr3[j];
-          arr3[j] = temp;
-        }
+  public static void fizzBuzz(int n) {
+    String[] solution = new String[n];
+    for (int i = 1; i <= n; i++) {
+      if (i % 3 == 0 && i % 5 == 0) {
+        solution[i-1] = "FizzBuzz";
+      } else if (i % 3 == 0) {
+        solution[i-1] = "Fizz";
+      } else if (i % 5 == 0) {
+        solution[i-1] = "Buzz";
+      } else {
+        solution[i-1] = Integer.toString(i);
       }
     }
-    System.out.println(Arrays.toString(arr3));
+    System.out.println(Arrays.toString(solution));
   }
 
   public static void main(String[] args) {
-    int[] array1 = { 3, 2, 1 };
-    int[] array2 = { 6, 5, 4 };
-    mergeAndSort(array1, array2);
+    fizzBuzz(15);
   }
 }
