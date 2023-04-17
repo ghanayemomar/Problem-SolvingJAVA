c0994fdc2dd5aa25376a8a7cac4afb6d66cc1d6b
gem00n <o.ghanaiem@student.aaup.edu>
2023-04-18 01:35:49 +0300

    4.Reverse String:

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,19 @@
-import java.util.*;
-import java.util.logging.LogManager;
+import java.util.Arrays;
 
-class Main {
-  public static void longestWord(String str) {
-    String longestWord="";
-    String [] arrayOfString = str.split(" ");
-    for(int i=0;i<arrayOfString.length;i++){
-      if(arrayOfString[i].length()>longestWord.length()){
-        longestWord = arrayOfString[i];
-      }
+class Main{
+  public static void reverseString(String str){
+    String [] myString = str.split("");
+    String temp = "";
+    int last=myString.length-1;
+    for(int i=0;i<myString.length/2;i++){
+      temp = myString[i];
+      myString[i]=myString[last-i];
+      myString[last-i]= temp;
     }
-    System.out.println(longestWord);
-    
+    System.out.println(Arrays.toString(myString));
   }
-
-  public static void main(String[] args) {
-    String str = "Hello Omar How Are You Ghanayem";
-    longestWord(str);
+  public static void main(String[] args){
+    String myString = "Hello World";
+    reverseString(myString);
   }
-
 }
